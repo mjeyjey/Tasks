eece6caa76df0059f8db3b24537fad9dacd0da32
mjeyjey <michaeljamesraya2005@gmail.com>
2025-06-23 04:28:19 +0000

    day 14 ata

--- Task2.java
+++ Task2.java
@@ -1,1 +1,17 @@
 
+public class Task2{
+    public static void main(String[] args){
+        byte three = 3;
+        short one = 1;
+        int zero = 0;
+        // String s = " ";
+        char w ='w';
+        char r = 'r';
+        char l = 'l';
+        char d = 'd';
+        float num = 2.0f;
+        boolean t = true;
+        String output = "H" + three + one + one + zero + " " + w + zero + r + l + d + " " + num + " " + t;
+        System.out.println(output);
+    }
+}
